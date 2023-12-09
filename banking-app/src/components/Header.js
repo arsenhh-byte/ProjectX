eaf2fbb3dfa6fbcbef8819f763b1cda26efaef02
Arsen Hendricks <arsenhendricks@gmail.com>
2023-12-09 14:37:33 +0300

    Update brand name in the header.

--- banking-app/src/components/Header.js
+++ banking-app/src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
     <header className="bg-gray-800 text-white">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <h1 className="flex title-font font-medium items-center text-white mb-4 md:mb-0">
-<span className="ml-3 text-2xl font-semibold">Arsen Premier Banking</span> {/* Updated Brand Name with new style */}
+<span className="ml-3 text-2xl font-semibold">Our Premier Banking</span> {/* Updated Brand Name with new style */}
         </h1>
         <NavBar />
       </div>
