fdcf149bfe239b019c1bf8f84d8e836ca327f82b
iWB-Git <sbp1784@gmail.com>
2023-12-09 14:18:00 +0300

    changed wordings for the bank name

--- banking-app/src/components/Header.js
+++ banking-app/src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
     <header className="bg-gray-800 text-white">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <h1 className="flex title-font font-medium items-center text-white mb-4 md:mb-0">
-          <span className="ml-3 text-xl font-bold">Arsen's Bank</span> {/* Updated Brand Name */}
+          <span className="ml-3 text-xl font-bold">Our Bank</span> {/* Updated Brand Name */}
         </h1>
         <NavBar />
       </div>
