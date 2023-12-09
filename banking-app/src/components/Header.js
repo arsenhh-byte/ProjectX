558ca13944a280c58cba355a7529fa083042c493
Arsen Hendricks <arsenhendricks@gmail.com>
2023-12-09 14:44:39 +0300

    bug found on line 15 missing colons:

--- banking-app/src/components/Header.js
+++ banking-app/src/components/Header.js
@@ -2,16 +2,16 @@ import React from "react";
 import NavBar from "./NavBar"; // Assuming NavBar is appropriate for a banking app
 
 const Header = () => {
-  return (
-    <header className="bg-gray-800 text-white">
-      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <h1 className="flex title-font font-medium items-center text-white mb-4 md:mb-0">
+    return (
+        <header className="bg-gray-800 text-white">
+            <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+                <h1 className="flex title-font font-medium items-center text-white mb-4 md:mb-0">
 
-<span className="ml-3 text-2xl font-semibold">Our Premier Banking</span> {/* Updated Brand Name with new style */}
-          <span className="ml-3 text-xl font-bold">Not My Bank</span> {/* Updated Brand Name */}
-<span className="ml-3 text-2xl font-semibold">Arsen Premier Banking</span> {/* Updated Brand Name with new style */}
-        </h1>
-        <NavBar />
-      </div>
-    </header>
+                    <span className="ml-3 text-2xl font-semibold">Our Premier Banking</span> {/* Updated Brand Name with new style */}
+                </h1>
+                <NavBar />
+            </div>
+        </header>
+    );
+};
 export default Header;
